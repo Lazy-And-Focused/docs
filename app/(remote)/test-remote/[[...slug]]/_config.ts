209422b3e78dbc2cfeaa7a55eae25524237d556b
lanvalird <strazhognay@gmail.com>
2025-09-21 22:47:58 +0400

    -add type `RemoteConfig` #7

--- app/(remote)/test-remote/[[...slug]]/_config.ts
+++ app/(remote)/test-remote/[[...slug]]/_config.ts
@@ -1,6 +1,21 @@
-export const remotes = [
+
+export type RemoteConfig = {
+    localUrl: string;
+    url: {
+      user: string;
+      repo: string;
+      branch: string;
+      docsPath: string;
+    };
+    files: string[];
+}
+
+/**
+ * Пока что это служит только для тестов, так что будет улучшаться
+ */
+export const remotes: RemoteConfig[] = [
   {
-    localUrl: 'test-remote',
+    localUrl: "test-remote",
     url: {
       user: "Lazy-And-Focused",
       repo: "BAD-template",
